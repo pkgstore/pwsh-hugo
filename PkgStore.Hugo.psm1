03c0cc107f7bd6b09369a84c6b5f32018f857ce8
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-07 05:25:11 +0300

    2023-01-07 02:25:11

--- PkgStore.Hugo.psm1
+++ PkgStore.Hugo.psm1
@@ -23,14 +23,59 @@ function Install-Hugo() {
     [string]${Version}
   )
 
+  ${VER} = "${Version}"
   ${DIR} = "${PSScriptRoot}\App"
-  ${ZIP} = "hugo_extended_${Version}_windows-amd64.zip"
-  ${URL} = "https://github.com/gohugoio/hugo/releases/download/v${Version}/${ZIP}"
+  ${ZIP} = "hugo_extended_${VER}_windows-amd64.zip"
+  ${URL} = "https://github.com/gohugoio/hugo/releases/download/v${VER}/${ZIP}"
 
   if ( Test-Path -Path "${DIR}" ) { Remove-Item -Path "${DIR}" -Recurse -Force }
 
+  Write-Information -MessageData "Create directory: '${DIR}'..." -InformationAction "Continue"
   New-Item -Path "${DIR}" -ItemType "Directory" -Force | Out-Null
+
+  Write-Information -MessageData "Download Hugo Extended: '${ZIP}'..." -InformationAction "Continue"
   Invoke-WebRequest "${URL}" -OutFile "${DIR}\${ZIP}"
+
+  Write-Information -MessageData "Expand: '${ZIP}'..." -InformationAction "Continue"
   Expand-Archive -Path "${DIR}\${ZIP}" -DestinationPath "${DIR}"
+
+  Write-Information -MessageData "Remove: '${ZIP}'..." -InformationAction "Continue"
   Remove-Item -Path "${DIR}\${ZIP}";
+}
+
+function Start-HugoServer() {
+  <#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .PARAMETER Version
+
+    .EXAMPLE
+
+    .LINK
+      Package Store: https://github.com/pkgstore
+
+    .NOTES
+      Author: Kitsune Solar <mail@kitsune.solar>
+  #>
+
+  [CmdletBinding()]
+
+  Param(
+    [Alias('P')]
+    [string]${Port},
+
+    [Alias('CD')]
+    [string]${CacheDir} = "$( Get-Location )\cache"
+  )
+
+  ${APP} = "${PSScriptRoot}\App\hugo.exe"
+  ${CMD} = @( "server", "-D", "-p ${Port}", "--printI18nWarnings", "--cacheDir ${CacheDir}", "--gc" )
+
+  if ( -not ( Test-Path -Path "${APP}" -PathType "Leaf" ) ) {
+    Write-Error -Message "'hugo.exe' not found! Please install Hugo: 'Install-Hugo'." -ErrorAction "Stop"
+  }
+
+  & "${APP}" ${CMD}
 }
